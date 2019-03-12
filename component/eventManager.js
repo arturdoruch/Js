1c6168323c52a8ebe2623f87710cb78620e93ff7
Artur Doruch <arturdoruch@interia.pl>
2019-03-12 14:23:56 +0100

    [component/eventManager] Added "one" method. Reorganize code

--- component/eventManager.js
+++ component/eventManager.js
@@ -3,26 +3,7 @@
     'use strict';
 
     var _events = {},
-        jQueryObject = $({}),
-        getEvent = function (id) {
-            return _events[id] ? _events[id] : null;
-        };
-
-    /**
-     * @param {string}   name The event name
-     * @param {jQuery|HTMLElement} element
-     * @param {function} listener
-     * @param {[]}       [listenerArgs]
-     * @param {object}   [listenerContext]
-     * @constructor
-     */
-    function _Event(name, element, listener, listenerArgs, listenerContext) {
-        this.name = name;
-        this.element = $(element);
-        this.listener = listener;
-        this.listenerArgs = listenerArgs || [];
-        this.listenerContext = listenerContext || window;
-    }
+        jQueryObject = $({});
 
     var eventManager = {
         /**
@@ -39,7 +20,7 @@
          * Removes listener or listeners for given event.
          *
          * @param {string} eventName
-         * @param {function} [listener] Removes given listener (if exist) or all listener if not given.
+         * @param {function} [listener] Removes given listener (if exists) or all listener if not given.
          */
         removeListener: function (eventName, listener) {
             jQueryObject.off(eventName, listener);
@@ -56,44 +37,50 @@
         },
 
         /**
-         * Adds event listener to HTML element.
+         * Attach an event listener function for one or more events to the elements.
          *
          * @param {string}   events The event or events name separated by coma.
-         * @param {jQuery|HTMLElement} element
+         * @param {jQuery|HTMLElement|string} element
          * @param {function} listener The event handler
          * @param {[]}       [listenerArgs = []]
          * @param {object}   [listenerContext = window]
          * @param {bool}     [preventDefault = true]
          *
-         * @return {string} The event Id
+         * @return {string|null} The event Id
          */
         on: function (events, element, listener, listenerArgs, listenerContext, preventDefault) {
-            if (typeof listener !== 'function') {
-                throw new Error ('The event listener must be a function.');
+            var event = prepareEvent(events, element, listener, listenerArgs, listenerContext, preventDefault);
+
+            if (!event) {
+                return null;
             }
 
-            var event = new _Event(events, element, listener, listenerArgs, listenerContext),
-                id = '#' + Object.keys(_events).length,
-                _listener = function (e) {
-                    if (preventDefault !== false) {
-                        e.preventDefault();
-                    }
+            event.$element.on(event.name, event.listener);
 
-                    listener.apply(event.listenerContext, [e].concat(event.listenerArgs));
-                };
-
-            if (event.element.length === 0) {
-                return;
-            }
-
-            event.element.off(event.name, _listener);
-            event.element.on(event.name, _listener);
-            // Set real event listener.
-            event.listener = _listener;
-
+            var id = '#' + Object.keys(_events).length;
             _events[id] = event;
 
             return id;
+        },
+
+        /**
+         * Attach a listener to an event for the elements. The listener is executed at most once per element per event type.
+         *
+         * @param {string}   events The event or events name separated by coma.
+         * @param {jQuery|HTMLElement|string} element
+         * @param {function} listener The event handler
+         * @param {[]}       [listenerArgs = []]
+         * @param {object}   [listenerContext = window]
+         * @param {bool}     [preventDefault = true]
+         */
+        one: function (events, element, listener, listenerArgs, listenerContext, preventDefault) {
+            var event = prepareEvent(events, element, listener, listenerArgs, listenerContext, preventDefault);
+
+            if (!event) {
+                return;
+            }
+
+            event.$element.one(event.name, event.listener);
         },
 
         /**
@@ -106,7 +93,7 @@
 
             if (event) {
                 delete _events[eventId];
-                event.element.off(event.name, event.listener);
+                event.$element.off(event.name, event.listener);
             }
         },
 
@@ -116,7 +103,7 @@
         trigger: function (eventId) {
             var event = getEvent(eventId);
             if (event) {
-                event.element.trigger(event.name);
+                event.$element.trigger(event.name);
             }
         },
 
@@ -127,6 +114,64 @@
             return _events;
         }
     };
+
+    /**
+     * Prepares event object.
+     *
+     * @param {string}   events The event or events name separated by coma.
+     * @param {jQuery|HTMLElement|string} element
+     * @param {function} listener The event handler
+     * @param {[]}       [listenerArgs = []]
+     * @param {object}   [listenerContext = window]
+     * @param {bool}     [preventDefault = true]
+     *
+     * @return {_Event|null}
+     */
+    function prepareEvent(events, element, listener, listenerArgs, listenerContext, preventDefault) {
+        if (typeof listener !== 'function') {
+            throw new Error('The event listener must be a function.');
+        }
+
+        var event = new _Event(events, element, listener, listenerArgs, listenerContext),
+            id = '#' + Object.keys(_events).length,
+            _listener = function (e) {
+                if (preventDefault !== false) {
+                    e.preventDefault();
+                }
+
+                listener.apply(event.listenerContext, [e].concat(event.listenerArgs));
+            };
+
+        if (event.$element.length === 0) {
+            return null;
+        }
+        // Set real event listener.
+        event.listener = _listener;
+        event.$element.off(event.name, event.listener);
+
+        return event;
+    }
+
+    function getEvent(id) {
+        return _events[id] ? _events[id] : null;
+    }
+
+    /**
+     * @param {string}   name The event name
+     * @param {jQuery|HTMLElement} element
+     * @param {function} listener
+     * @param {[]}       [listenerArgs]
+     * @param {object}   [listenerContext]
+     * @constructor
+     */
+    function _Event(name, element, listener, listenerArgs, listenerContext) {
+        this.name = name;
+        this.$element = $(element);
+        this.listener = listener;
+        this.listenerArgs = listenerArgs || [];
+        this.listenerContext = listenerContext || window;
+    }
+
 
     if (typeof define === 'function' && define.amd) {
         define(function() {
